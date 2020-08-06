7fafc0a3df59820f4e9ae3eeb1575625c968626c
CindyKatoni <katmuema@gmail.com>
2020-08-06 18:26:22 +0300

    edit button removed, click note to edit

--- js/script.js
+++ js/script.js
@@ -24,14 +24,14 @@
 	 * It will be used in the addNote and deleteNote functions.
 	 */
 	function displayNotes( array ) {
-		$("#notes-display").html("");
+		$("ul#notes-display").html("");
 
 		/**Add each note in the notesArray to the unordered list with id #notes-display
 		 * Give each list item an id with the format id="note-<indexOf(note)>". 
 				 E.g the first note <li id="0"> and so on.
 		 */
 		return array.forEach(function (note, index) {
-			$("#notes-display").prepend(function () {
+			$("ul#notes-display").prepend(function () {
 				return `<li id=${index} class="list-group-item d-flex justify-content-between align-items-center">
 						<span class="note">${note}</span>
 						<span class="badge badge-danger badge-pill delete">
@@ -42,7 +42,7 @@
 		});
 	}
 
-	$("#write-note").click(function (event) {
+	$("button#write-note").click(function (event) {
 		event.preventDefault();
 
 		const addedNote = $("textarea#note-content").val();
@@ -65,14 +65,14 @@
 	 * The note's id - 1 = the index of the note in the notesArray
 	 * Use .splice() method to remove the note from the array 
 	 */
-	$('#notes-display').on('click', 'li > .delete', function () {
+	$('ul#notes-display').on('click', 'li > .delete', function () {
 		const noteIndex = parseInt($(this).parent().attr('id'));
 
 		// Delete the note from the noteArray
 		notesArray.splice(noteIndex, 1);
 
 		// clear the textarea
-		$("#note-content").val("");
+		$("textarea#note-content").val("");
 		
 		//  Show the notes in the HTML
 		displayNotes(notesArray);
@@ -84,16 +84,17 @@
 	 * Display the note in the text area
 	 * After editing the note, replace it into the notesArray using .splice() at its index
 	 * .splice() can add new elements, replace an element or delete an element from an array*/ 
-	$('#notes-display').on('click', 'li > .note', function () {
+	$('ul#notes-display').on('click', 'li > .note', function () {
 		// remove the add note button and replace it with the edit note button
-		$('#display-add-button').html(``);
-		$('#display-edit-button').html(`
+		$('span#display-add-button').html(``);
+		$('span#display-edit-button').html(`
 			<button class="bg-yellow-500 hover:bg-yellow-400 text-black font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded "
 			type="button" id="edit-note">
 				<i class="fas fa-pen"></i> Edit Note
 			</button>
 		`)
 
+		
 		// Get the content of the note
 		const note = $(this).text();
 
@@ -101,10 +102,10 @@
 		editedNoteIndex = parseInt($(this).parent().attr('id'));
 
 		// Place the note content in the textarea
-		$('#note-content').val(note);
+		$('textarea#note-content').val(note);
 
 		//The newly edited note
-		editedNote = $('#note-content').val();
+		editedNote = $('textarea#note-content').val();
 
 		console.log(editedNote, editedNoteIndex);
 	});
@@ -112,7 +113,7 @@
 	/** Bind the click event to the edit note button
 		 * Add the edited note to the notesArray at its original index
 		 */
-		$("#display-edit-button").on('click', '#display-edit-button> #edit-note', function() {
+		$("span#display-edit-button").on('click', 'span#display-edit-button> button#edit-note', function() {
 			console.log('editing note');
 			//replace the old note at its index 
 		// notesArray.splice(noteIndex, 1, editedNote);
